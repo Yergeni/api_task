05f7c9ae2fd74fe0a4f069e6274566fc23f27ca1
Yergeni <yergenis@gmail.com>
2018-07-02 22:37:08 -0400

    First commit. Preparing to be pushed to a remote repo

---
+++ api_task.py
@@ -0,0 +1,87 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+cars = [
+    {
+        "id": 1,
+        "model": "Honda Civic",
+        "year": 2017,
+        "price": "$30000"
+    },
+    {
+        "id": 2,
+        "model": "Mitsubishi Lancer",
+        "year": 2006,
+        "price": "$3500"
+    },
+    {
+        "id": 3,
+        "model": "Toyota Corolla",
+        "year": 2018,
+        "price": "$18000"
+    }
+]
+
+class Car():
+
+    @app.route('/cars')
+    def get_cars():
+        return jsonify(cars)
+
+    @app.route('/cars/<int:id>')
+    def get_car(id):
+        trying to verify if id was entered
+        if 'id' in request.args:
+            car_id = request.args.get('id')
+        return "Bad request", 404
+            
+        for car in cars:
+            if(car_id == car["id"]):
+                return jsonify(car), 200
+        return "car not found", 404
+    
+
+    @app.route('/cars', methods=['POST'])
+    def add_car():
+        car_obj = request.get_json()
+        car_id = car_obj["id"]
+        for car in cars:
+            if(car_id == car["id"]):
+                return "car with id {} already exists".format(car_id), 400
+
+        cars.append(request.get_json())
+        return "", 201
+
+    @app.route('/cars', methods=['PUT'])
+    def update_car():
+        car_obj = request.get_json()
+        car_id = car_obj["id"]
+        for car in cars:
+            if(car_id == car["id"]):
+                car["model"] = car_obj["model"]
+                car["year"] = car_obj["year"]
+                car["price"] = car_obj["price"]
+                # If requested car exist; return the updated car with HTTP Response 200.
+                return jsonify(car), 200
+        
+        # Otherwise, create the car and send a HTTP Response 201.
+        cars.append(car_obj)
+        return jsonify(car_obj), 201
+
+    @app.route('/cars/<int:id>', methods=['DELETE'])
+    def delete_car(id):
+        # trying to verify if id was entered
+        if 'id' in request.args:
+            car_id = request.args.get('id')
+        return "Bad request", 404
+
+        for car in cars:
+            if(car_id == car["id"]):
+                cars.remove(car)
+        return "car not found", 404
+
+        return "Car with id: {} have been deleted.".format(id), 200
+      
+if __name__ == "__main__":  
+    app.run(debug=True)
